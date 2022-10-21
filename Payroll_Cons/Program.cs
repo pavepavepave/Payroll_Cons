cb5939c977c0ff3e2b1955851f08e3b874e5a5d6
Павел <deadamag@mail.ru>
2022-10-21 23:56:30 +0300

    v.1.0

--- Payroll_Cons/Program.cs
+++ Payroll_Cons/Program.cs
@@ -5,16 +5,31 @@ public class MainClass
 {
     static void Main()
     {
+        Console.Write("Введите количество работников: ");
+        Employee[] worker = new Employee[int.Parse(Console.ReadLine())];
 
-        Employee firstEmployee = new Employee();
-        Console.Write("Введите ФИО сотрудника: ");
-        firstEmployee.EmployeeName = Console.ReadLine();
-        Console.Write("Введите з/п сотрудника: ");
-        firstEmployee.Salary = double.Parse(Console.ReadLine());
-        Console.Write("Введите сумму вычета за ребенка: ");
-        firstEmployee.ChildDeduction = double.Parse(Console.ReadLine());
-        
-        firstEmployee.PrintResult();
+        for (int i = 0; i < worker.Length; ++i)
+        {
+            Employee firstEmployee = new Employee();
+            Console.Write("Введите ФИО сотрудника: ");
+            firstEmployee.EmployeeName = Console.ReadLine();
+            Console.Write("Введите з/п сотрудника: ");
+            firstEmployee.Salary = double.Parse(Console.ReadLine());
+            Console.Write("Введите сумму вычета за ребенка: ");
+            firstEmployee.ChildDeduction = double.Parse(Console.ReadLine());
+
+            firstEmployee.PrintResult();
+        }
+        Console.ReadKey();
+
+
+
+
+
+
+
+
+      
 
 
 
