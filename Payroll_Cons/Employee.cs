d3a1f1c054b46fe1d07a9232169f282cbc2666d9
deadamag <deadamag@mail.ru>
2022-10-21 12:13:35 +0300

    Добавьте файлы проекта.

---
+++ Payroll_Cons/Employee.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+internal class Employee
+{
+    public string EmployeeName { get; set; } //имя работника
+    private const double MinimumWage = 13890; //мрот
+    public double Salary { get; set; } // зарплата
+    public double ChildDeduction { get; set; } //вычет за ребенка
+    private double TaxesSum = 0;
+
+    public void Taxes (double Salary,  double ChildDeduction) //налог
+    {
+        TaxesSum = Math.Round(((Salary - ChildDeduction) * 13 / 100),0);
+    } 
+
+   public void PrintResult()
+    {
+        Console.ForegroundColor = ConsoleColor.Yellow;   
+        Console.WriteLine(($"Работник: {EmployeeName}"));
+        Console.WriteLine($"Сумма налога к удержанию: {TaxesSum}");
+        Console.WriteLine($"Денежная выплата работнику:" + (Salary - TaxesSum));
+        Console.WriteLine("ОПС: ...");
+        Console.WriteLine("ОМС: ...");
+        Console.WriteLine("ВНиМ: ...");
+        Console.WriteLine("ФССС: ...");
+    }
+
+
+
+
+
+
+
+}
+
---
+++ Payroll_Cons/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Xml.Linq;
+
+
+public class MainClass
+{
+    static void Main()
+    {
+
+        Employee firstEmployee = new Employee();
+        Console.Write("Введите ФИО сотрудника: ");
+        firstEmployee.EmployeeName = Console.ReadLine();
+        Console.Write("Введите з/п сотрудника: ");
+        firstEmployee.Salary = double.Parse(Console.ReadLine());
+        Console.Write("Введите сумму вычета за ребенка: ");
+        firstEmployee.ChildDeduction = double.Parse(Console.ReadLine());
+        firstEmployee.Taxes(firstEmployee.Salary, firstEmployee.ChildDeduction);
+        firstEmployee.PrintResult();
+
+
+        
+
+
+
+    }
+}
